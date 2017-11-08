120f91e73866b0511558e5c877afa80e3e6f4fe4
SmellydogCoding <smellydogcoding@gmail.com>
2017-11-08 18:39:25 +0000

    section 1-11/17

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,4 +1,20 @@
-<template>
-    <h1>The User Page</h1>
-
+<template lang="pug">
+  div
+    h1 The User Page
+    hr
+    p User: {{ id }}
+    button.btn.btn-primary(@click="goHome()") Go Home
 </template>
+
+<script>
+  export default {
+    props: {
+      id: {}
+    },
+    methods: {
+      goHome() {
+        this.$router.push('/');
+      }
+    }
+  }
+</script>
